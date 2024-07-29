08a96533f2305d17efafecb395c2c447ead5f470
Kole <koladevops@outlook.com>
2024-07-29 11:30:09 +0100

    build code yml add

--- scripts/install_dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-sudo apt-get update
-sudo apt-get install -y maven git
-ipt
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 echo "Starting GeoStore application..."
 
-# Stop any running instance of GeoStore
-sudo pkill -f 'java -jar /var/www/html/geostore.war'
-
-# Start the GeoStore application
-nohup java -jar /var/www/html/geostore.war &> /var/www/html/nohup.out &
-
-# Ensure the application is running
-if pgrep -f 'java -jar /var/www/html/geostore.war'; then
-    echo "GeoStore application started successfully."
-else
-    echo "Failed to start GeoStore application. Check logs for details."
-    exit 1
-fi
+sudo systemctl restart apache2
